39bfbb28fed8d9d779e9f6fb95ef49cd69d418ce
Yuki KAN <re@pixely.jp>
2015-03-31 23:56:49 +0900

    first version

---
+++ handlers/processor.js
@@ -0,0 +1,39 @@
+﻿'use strict';
+
+var vm = require('vm');
+var formidable = require('formidable');
+
+module.exports = {
+    post: function (req, res) {
+        
+        var sandbox = {
+            result: '',
+            code  : 'return "";',
+            errors: []
+        };
+        
+        if (req.query.json) {
+            try {
+                sandbox.json = JSON.parse(req.query.json);
+            } catch (e) {
+                res.status(400);
+                sandbox.errors.push(e.toString());
+            }
+        }
+        
+        sandbox.code = req.query.code;
+        
+        console.log(sandbox);
+
+        var code = 'result = (function () { ' + sandbox.code + ' }());';
+        
+        try {
+            vm.runInNewContext(code, sandbox);
+        } catch (e) {
+            res.status(400);
+            sandbox.errors.push(e.toString());
+        }
+        
+        res.json(sandbox);
+    }
+};
---
+++ server.js
@@ -0,0 +1,54 @@
+﻿'use strict';
+
+var express = require('express');
+var morgan = require('morgan');
+var bodyParser = require('body-parser')
+var swaggerize = require('swaggerize-express');
+
+var apiapp = require('./apiapp.json');
+var apidef = require('./apidef.json');
+
+// complete apidef.json
+if (process.env.WEBSITE_HOSTNAME) {
+    apidef.host = process.env.WEBSITE_HOSTNAME;
+}
+
+// map apidef.json to apiapp.json
+apiapp.version = apidef.info.version;
+apiapp.author = apidef.info.contact.name;
+apiapp.title = apidef.info.title;
+apiapp.summary = apidef.info.description;
+
+apiapp.license = {
+    type: apidef.info.license.name,
+    url: apidef.info.license.url
+};
+
+apiapp.links.push({
+    text: apidef.info.contact.name,
+    url: apidef.info.contact.url
+});
+
+var app = express();
+
+app.use(morgan('combined'));
+
+app.use(bodyParser.json({ type: '*/*' }));
+
+app.use(swaggerize({
+    api: apidef,
+    docspath: apiapp.endpoints.apiDefinition,
+    handlers: './handlers'
+}));
+
+app.get('/apiapp.json', function (req, res) {
+    res.json(apiapp);
+});
+
+app.get('/', function (req, res) {
+    
+    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+    res.end('JavaScript Processor API App.');
+});
+
+app.listen(process.env.PORT || 1337);
